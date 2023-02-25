4e2956e431adf9c7fc2f550073e5bed4108f5c63
ajay <ap221882@gmail.com>
2023-02-25 20:44:02 +0530

    feat: renaming to Sentinel

--- DataStructuresVisualises/class2n3n4n5/LinkedList/SentinentalLinkedList.ts
+++ DataStructuresVisualises/class2n3n4n5/LinkedList/SentinentalLinkedList.ts
@@ -1,6 +1,6 @@
-class SentinentalNode {
-  prev: null | SentinentalNode;
-  next: SentinentalNode | null;
+class SentinelNode {
+  prev: null | SentinelNode;
+  next: SentinelNode | null;
   value: number;
 
   constructor(value: number) {
@@ -12,14 +12,16 @@ class SentinentalNode {
 
 //~> SentinentalList have a head and a tail as default and we add values to it. Values can be deleted and added from both ends hence
 
-class SentinentalLinkedList {
-  head: null | SentinentalNode;
-  tail: null | SentinentalNode;
-  constructor() {
-    this.head = new SentinentalNode(null);
-    this.tail = new SentinentalNode(null);
+class SentinelLinkedList {
+  head: null | SentinelNode;
+  tail: null | SentinelNode;
+  constructor(...values: number[]) {
+    this.head = new SentinelNode(null);
+    this.tail = new SentinelNode(null);
     this.head.next = this.tail;
     this.tail.prev = this.head;
+
+    this.insertValues(this.head, values);
   }
 
   //~# It is worth noting that IN assignment operator (=), RHS is indicating node, LHS is the pointer to node (next or previous)                       ;
@@ -37,63 +39,67 @@ class SentinentalLinkedList {
     return null;
   }
 
-  insertValue(node: SentinentalNode, value: number) {
-    // const searchedNode = this.search(value);
+  insertValues(node: SentinelNode, values: number[]) {
+    values.forEach((value) => {
+      const spot = new SentinelNode(value);
 
-    const spot = new SentinentalNode(value);
+      spot.next = node.next;
+      spot.prev = node;
 
-    spot.next = node.next;
-    spot.prev = node;
-
-    spot.next.prev = spot.prev.next = spot;
+      spot.next.prev = spot.prev.next = spot;
+    });
   }
 
-  addANodeToBack(value: number) {
-    this.insertValue(this.tail.prev, value);
+  addNodesToBack(...values: number[]) {
+    this.insertValues(this.tail.prev, values);
   }
 
   //~^ MAN CLIMBING THE WALL                        ;
-  addANodeToFront(value: number) {
-    this.insertValue(this.head, value);
+  addNodesToFront(...values: number[]) {
+    this.insertValues(this.head, values);
   }
 
-  insertAfter(insertedAfter: number, insertValue: number) {
+  insertAfter(insertedAfter: number, ...insertValues: number[]) {
     const searchedNode = this.search(insertedAfter);
-    this.insertValue(searchedNode, insertValue);
+    this.insertValues(searchedNode, insertValues);
   }
 
-  insertBefore(insertedBefore: number, insertValue: number) {
+  insertBefore(insertedBefore: number, ...insertValues: number[]) {
     const searchedNode = this.search(insertedBefore);
-    this.insertValue(searchedNode.prev, insertValue);
+    this.insertValues(searchedNode.prev, insertValues);
   }
 
-  deleteANode(value: number) {
-    for (
-      let current = this.head.next;
-      current != this.tail;
-      current = current.next
-    ) {
-      //~! a.b.c means (a.b).c => a.b evaluates to a node and that is assigned a pointer to any other node
-      if (current.value == value) {
-        //> current.prev is evaluated to previous node and it's next pointer is to the current's next pointer node
-        current.prev.next = current.next;
-        //> current.next is evaluated to next node and it's previous pointer is to the current's previous pointer node
-        current.next.prev = current.prev;
+  deleteNodes(...values: number[]) {
+    values.forEach((value) => {
+      for (
+        let current = this.head.next;
+        current != this.tail;
+        current = current.next
+      ) {
+        //~! a.b.c means (a.b).c => a.b evaluates to a node and that is assigned a pointer to any other node
+        if (current.value == value) {
+          //> current.prev is evaluated to previous node and it's next pointer is to the current's next pointer node
+          current.prev.next = current.next;
+          //> current.next is evaluated to next node and it's previous pointer is to the current's previous pointer node
+          current.next.prev = current.prev;
+        }
       }
-    }
+    });
   }
 
-  removeAllOccurrences = (value: number) => {
-    for (
-      let current = this.head.next;
-      current != this.tail;
-      current = current.next
-    ) {
-      if (current.value == value) {
-        current.next.prev = current.prev;
-        current.prev.next = current.next;
+  removeAllOccurrences = (...values: number[]) => {
+    values.forEach((value) => {
+      for (
+        let current = this.head.next;
+        current != this.tail;
+        current = current.next
+      ) {
+        if (current.value == value) {
+          current.next.prev = current.prev;
+          current.prev.next = current.next;
+        }
       }
-    }
+    });
   };
 
   printForward() {
@@ -117,19 +123,16 @@ class SentinentalLinkedList {
   }
 }
 
-const senti = new SentinentalLinkedList();
-senti.addANodeToFront(3);
-senti.addANodeToFront(5);
-senti.addANodeToFront(4);
+const senti = new SentinelLinkedList(11, 12, 13);
+// senti.addANodeToFront(3);
+// senti.addANodeToFront(5);
+// senti.addANodeToFront(4);
 
-senti.insertAfter(4, 99);
+// senti.insertAfter(4, 99);
 
-senti.insertBefore(4, 99);
+// senti.insertBefore(4, 99);
 
-// senti.addANodeToFront(78);
-// senti.printForward();
-// senti.printBackward();
-// senti.deleteANode(78);
-// senti.removeAllOccurrences(4);
+// senti.addNodesToBack(2, 3, 4);
+senti.addNodesToFront(2, 3, 4);
 
 senti.printForward();
